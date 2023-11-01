b7936d75e3a0f4b0f37b94d310befde3f5b1b691
NRI酒井 将大 <m2-sakai@nri.co.jp>
2023-11-01 16:23:22 +0900

    [fix] management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,18 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '@/app/ui/globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Manage Weight App",
-    default: "Manage Weight App",
+    template: '%s | Manage Weight App',
+    default: 'Manage Weight App',
   },
-  description: "Manage Weight App built with App Router",
+  description: 'Manage Weight App built with App Router',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
